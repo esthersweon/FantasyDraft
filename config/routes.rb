af73cd8fc8bbb8e633ab8bd96c3bd2deffcc67d2
ellismitchell <elligmitchell@gmail.com>
2017-09-28 13:35:07 -0700

    finished users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new', as: 'login'
   post 'sessions', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 end
